d695795e3c9e77d1130801bc7b5a95e29b5ae5c0
Abhishek Verma <helloabhii71@gmail.com>
2024-09-26 23:25:17 +0530

    add route

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.get('/', (req,res) => {
   res.send('Hello, World! CI/CD Pipeline is running!');
 });
 
+app.get('/test', (req, res) => {
+  res.send('Test route working fine!');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+module.exports = app;
